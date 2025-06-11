9f45416ed119de771aaa1d1e7e4ee52ee2028f81
Sumit Patil <145496112+Sumitp0112@users.noreply.github.com>
2025-06-11 19:39:50 +0530

    Create GreaterThan.java

---
+++ 1. operators/Relational/GreaterThan.java
@@ -0,0 +1,11 @@
+class GreterThan {
+
+    public static void main(String[] args)
+    {
+        // Initializing variables
+        int var1 = 10, var2 = 20;
+
+        // Comparing var1 and var2 and
+        System.out.println("var1 < var2: " + (var1 > var2));
+    }
+}
