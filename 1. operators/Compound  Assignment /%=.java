2070828f1842323f08efb7e854954136a73caaaa
Sumit Patil <145496112+Sumitp0112@users.noreply.github.com>
2025-06-11 19:24:39 +0530

    Create %=.java

---
+++ 1. operators/Compound  Assignment /%=.java
@@ -0,0 +1,18 @@
+
+class Assignment {
+    public static void main(String[] args)
+    {
+
+        // Declaring variables
+        int num1 = 5, num2 = 3;
+
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+        // Moduling & Assigning values
+        num1 %= num2;
+
+        // Displaying the assigned values
+        System.out.println("num1 = " + num1);
+    }
+}
