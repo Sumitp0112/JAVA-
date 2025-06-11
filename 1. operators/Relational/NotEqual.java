60cdae6e198fbf202a637acb96c78202b7a1778a
Sumit Patil <145496112+Sumitp0112@users.noreply.github.com>
2025-06-11 19:52:27 +0530

    Create NotEqual.java

---
+++ 1. operators/Relational/NotEqual.java
@@ -0,0 +1,9 @@
+class LessThanEqualTo {
+    public static void main(String[] args)
+    {
+        // Initializing variables
+        int var1 = 10, var2 = 10;
+        // Comparing var1 and var2
+        System.out.println("var1 <= var2: " + (var1 != var2));
+    }
+}
