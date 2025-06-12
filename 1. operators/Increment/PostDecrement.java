47d04c527741d046e54516df985e17fc19fec926
Sumit Patil <145496112+Sumitp0112@users.noreply.github.com>
2025-06-12 20:00:02 +0530

    Create PostDecrement.java

---
+++ 1. operators/Increment/PostDecrement.java
@@ -0,0 +1,10 @@
+// If a variable is suffixed with decrement operator are called as Post-Decrement operator.
+// Syntax = var--;
+// Here, the value(old) from the variable is substituted first and then the variable is subtracted by 1 value.
+
+class PostDecrement
+  {
+    int a = 10;
+    a--;
+    System.out.println(a);
+  }
