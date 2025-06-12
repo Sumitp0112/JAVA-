abbbcc0a87b02873c9fc7799cf6aa09f6faa7adf
Sumit Patil <145496112+Sumitp0112@users.noreply.github.com>
2025-06-12 19:18:32 +0530

    Create Condtional4.java

---
+++ 1. operators/Conditional /Condtional4.java
@@ -0,0 +1,17 @@
+//Smallest of 3 number
+
+class Condtional4
+  {
+    public static void main (String [] args)
+    {
+      int a1 = 10;
+      int a2 = 20;
+      int a3 = 30;
+
+      int ans = a1 < a2 ? a1 : a2;
+      System.out.println(ans);
+
+      int ans1 = a3 < ans ? ans : a3;
+      System.out.peintln(ans1);
+    }
+  }
