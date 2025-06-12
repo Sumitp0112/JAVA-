0bd35b70f6693cacc23891629797efd8dff71249
Sumit Patil <145496112+Sumitp0112@users.noreply.github.com>
2025-06-12 19:14:25 +0530

    Create Conditional3.java

---
+++ 1. operators/Conditional /Conditional3.java
@@ -0,0 +1,13 @@
+//Smallest of 2
+
+class Conditional3
+  {
+    public static void main(String [] args)
+    {
+      int a1 = 30;
+      int a2 = 35;
+
+      int ans = a1 < a1 ? a1 : a2;
+    }
+    System.out.println(ans);
+  }
