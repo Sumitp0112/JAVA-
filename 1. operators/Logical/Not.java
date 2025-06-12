42aba1283cc97301ea474783e8704cba02bd6549
Sumit Patil <145496112+Sumitp0112@users.noreply.github.com>
2025-06-12 19:39:30 +0530

    Create Not.java

---
+++ 1. operators/Logical/Not.java
@@ -0,0 +1,12 @@
+// it is a unary operator
+
+class LogicalNot {
+    public static void main(String[] args)
+    {
+        int a = 10, b = 1;
+      
+        // Using logical NOT operator
+        System.out.println("!(a < b) = " + !(a < b));
+        System.out.println("!(a > b) = " + !(a > b));
+    }
+}
