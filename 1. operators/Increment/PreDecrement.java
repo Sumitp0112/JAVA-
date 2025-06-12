959a08d0320dc8812f55c8e6513e8865508b9da4
Sumit Patil <145496112+Sumitp0112@users.noreply.github.com>
2025-06-12 19:57:51 +0530

    Create PreDecrement.java

---
+++ 1. operators/Increment/PreDecrement.java
@@ -0,0 +1,10 @@
+// If a variable is prefixed with decrement operator are called as Pre-Decrement operator.
+// Syntax = --var;
+// Here, first the variable will be subtracted by 1 value and then the updated value is substituted.
+
+class PreDecrement
+  {
+    int a = 10;
+    --a;
+    System.out.println(a);
+  }
