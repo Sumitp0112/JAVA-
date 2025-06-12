79c59c922d04b55c30235175a2ff5e69da59c05b
Sumit Patil <145496112+Sumitp0112@users.noreply.github.com>
2025-06-12 20:54:38 +0530

    If2.java

--- Control Flow Statement/1. Decision Making Statement/1. IF/If2.java
+++ Control Flow Statement/1. Decision Making Statement/1. IF/If2.java
@@ -1,3 +1,16 @@
+/*
+Syntax = if (condition)      
+         {
+            Statement;
+         }
+
+An if statement as a condition along with block with cantain statement. 
+The condition must be a boolean type.
+
+Control Flow of IF:
+          - The statement inside the if block gets executed only if condition evaluate the true.
+          - If condition evaluate is false the if block is skipped or ignored.
+*/
 import java.util.Scanner;
 class  If2
 {
