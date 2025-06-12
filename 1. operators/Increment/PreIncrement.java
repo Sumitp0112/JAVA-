e77f9b30826d6b962ec6ed08a58b08cee4b227bc
Sumit Patil <145496112+Sumitp0112@users.noreply.github.com>
2025-06-12 19:48:48 +0530

    Create PreIncrement.java

---
+++ 1. operators/Increment/PreIncrement.java
@@ -0,0 +1,10 @@
+// If a variable is prefixed with increment operator are called as Pre-increment operator.
+// Syntax = ++var;
+// Here, first the variable will be added by 1 value and then the updated value is substituted.
+
+class PreIncrement
+  {
+    int a = 10;
+    ++a;
+    System.out.println(a);
+  }
