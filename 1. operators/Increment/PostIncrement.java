b13a35c2c66d98858e218ee2d9bebe83200ccb06
Sumit Patil <145496112+Sumitp0112@users.noreply.github.com>
2025-06-12 19:55:27 +0530

    Create PostIncrement.java

---
+++ 1. operators/Increment/PostIncrement.java
@@ -0,0 +1,10 @@
+// If a variable is suffixed with increment operator are called as Post-increment operator.
+// Syntax = var++;
+// Here, the value(old) from the variable is substituted first and then the variable is updated with 1 value.
+
+class PostIncrement
+  {
+    int a = 10;
+    a++;
+    System.out.println(a);
+  }
