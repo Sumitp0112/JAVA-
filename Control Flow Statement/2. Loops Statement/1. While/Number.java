6a6189321bdc9a837bde41436239ff86332fced4
Sumit Patil <145496112+Sumitp0112@users.noreply.github.com>
2025-06-13 14:18:10 +0530

    Create Number.java

---
+++ Control Flow Statement/2. Loops Statement/1. While/Number.java
@@ -0,0 +1,12 @@
+class Number
+  {
+    public static void main(String [] args)
+    {
+      int num = 1;
+      while (num <= 10)
+        {
+          System.out.println(num) ;
+          num++ ;
+        }
+    }
+  }
