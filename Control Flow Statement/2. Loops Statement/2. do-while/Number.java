c6b1a0e7bc7014d7c35d43221d11a1017642080c
Sumit Patil <145496112+Sumitp0112@users.noreply.github.com>
2025-06-13 14:42:00 +0530

    Create Number.java

---
+++ Control Flow Statement/2. Loops Statement/2. do-while/Number.java
@@ -0,0 +1,16 @@
+// Print number 1 to 100
+
+class Number 
+{
+  public static void main(String [] args)
+  {
+    int num = 1;
+	  
+	  do
+	  {
+		  System.out.println(num) ;
+		  num++ ;
+	  }
+	  while (num <= 100);
+  }
+}
