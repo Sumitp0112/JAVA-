62037ead4e0cf988b405f7f040346179c86256d3
Sumit Patil <145496112+Sumitp0112@users.noreply.github.com>
2025-06-13 14:22:02 +0530

    Create Even.java

---
+++ Control Flow Statement/2. Loops Statement/1. While/Even.java
@@ -0,0 +1,16 @@
+// Print even number 1 to 100
+class Even
+  {
+    public static void main(String [] args)
+    {
+      int num = 1 ;
+      while (num <= 100)
+        {
+          if (num %2 == 0)
+          {
+            System.out.println(num);
+          }
+          num++ ;
+        }
+    }
+  }
