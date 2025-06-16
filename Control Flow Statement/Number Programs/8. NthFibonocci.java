e770405328f52cb7e142dfac21e689c30e2f4894
Sumit Patil <145496112+Sumitp0112@users.noreply.github.com>
2025-06-16 22:19:10 +0530

    Create 8. NthFibonocci.java

---
+++ Control Flow Statement/Number Programs/8. NthFibonocci.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Demo 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your number : ");
+		int num = sc.nextInt();
+		int n1 = 0,n2 = 1,n3 = 2;
+		for (int i = 0; i < num ; i++ )
+		{
+			n3 = n1 + n2;
+			n1 = n2;
+			n2 = n3;
+		}
+    	System.out.print(n1);
+	}
+}
