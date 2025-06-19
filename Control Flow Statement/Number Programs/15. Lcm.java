fec1d094648aa2cd1d7e8e3e0ab9d5b2946242bb
Sumit Patil <145496112+Sumitp0112@users.noreply.github.com>
2025-06-19 23:56:30 +0530

    Create 15. Lcm.java

---
+++ Control Flow Statement/Number Programs/15. Lcm.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Lcm 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the first number : ");
+		int n1 = sc.nextInt();
+		
+		System.out.println("Enter the second number : ");
+		int n2 = sc.nextInt();
+		int large = n1 > n2? n1:n2;
+		
+		int i = 1;
+		while (true)
+		{
+			if ((large * i)%n1 == 0 && (large * i)%n2 == 0)
+			{
+				System.out.println("LCM is : "+(large*i));
+				break;
+			}
+			i++;
+		}
+	}
+}
