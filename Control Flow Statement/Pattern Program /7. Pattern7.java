0bdbe930c94de6de08dec2bdb9a1b25d4304316f
Sumit Patil <145496112+Sumitp0112@users.noreply.github.com>
2025-07-01 00:06:57 +0530

    Create 7. Pattern7.java

---
+++ Control Flow Statement/Pattern Program /7. Pattern7.java
@@ -0,0 +1,21 @@
+class Pattern7
+{
+	public static void main(String[] args) 
+	{
+		int n = 7;
+		for (int i = 0;i<n ;i++ )
+		{
+			for (int j = 0;j<n ;j++ )
+			{
+				if (j==0||i==n-1||i==j)
+				{
+					System.out.print("*"+" ");
+				}
+				else{
+					System.out.print(" "+" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
