7c738844c6414a601b2038275ecc3f68f593f9f4
Sumit Patil <145496112+Sumitp0112@users.noreply.github.com>
2025-07-04 23:14:50 +0530

    Create 20. FrequencyOfEachDigit.java

---
+++ Control Flow Statement/Range of Number Program/20. FrequencyOfEachDigit.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class  FrequencyOfEachDigit
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your number : ");
+		int num = sc.nextInt();
+		
+		for (int i = 0;i<=9 ;i++ )
+		{
+			int count = 0;
+			int temp = num ;
+			
+			while (temp > 0)
+			{
+				if (i == temp % 10)
+				{
+					count++;
+				}
+				temp /= 10;
+			}
+			
+			if (count > 0)
+			{
+				System.out.println(i+" present in : "+count+" times");
+			}
+		}
+	}
+}
