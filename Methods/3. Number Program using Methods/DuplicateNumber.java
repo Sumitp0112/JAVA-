fcdf964665e5432b8510b348e7e7911a2adac356
Sumit Patil <145496112+Sumitp0112@users.noreply.github.com>
2025-07-17 23:03:25 +0530

    Create DuplicateNumber.java

---
+++ Methods/3. Number Program using Methods/DuplicateNumber.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class DuplicateNumber
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your number : ");
+		int num = sc.nextInt();
+		duplicateNum(num);
+	}
+	public static void duplicateNum(int num)
+	{
+		for (int i = 0;i<=9 ;i++ )
+		{
+			int count = 0;
+			int temp = num ;
+			
+			while (temp > 0)
+			{
+				if (i == temp % 10)
+				{
+					count++;
+				}
+				temp /= 10;
+			}
+			
+			if (count > 1)
+			{
+				System.out.println(i+" present in : "+count+" times");
+			}
+		}
+	}
+}
